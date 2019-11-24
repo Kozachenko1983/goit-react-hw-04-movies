4e200f50900abb51a81bd9b6d83ba532f4cab3d6
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-11-24 13:06:09 +0200

    Updates

---
+++ precache-manifest.74d45e1ae0ea4b5fa3a02e309f2f69e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d840a86a64946efd1a7d4794774843da",
+    "url": "/goit-react-hw-04-movies/./index.html"
+  },
+  {
+    "revision": "f07ea3786adeed80c7d4",
+    "url": "/goit-react-hw-04-movies/./static/css/main.c7cb0ca9.chunk.css"
+  },
+  {
+    "revision": "a98adcaf149ba7d027bf",
+    "url": "/goit-react-hw-04-movies/./static/js/2.bf7c8d3a.chunk.js"
+  },
+  {
+    "revision": "f07ea3786adeed80c7d4",
+    "url": "/goit-react-hw-04-movies/./static/js/main.0cd87777.chunk.js"
+  },
+  {
+    "revision": "3d814ea7c0eb2ea42ddf",
+    "url": "/goit-react-hw-04-movies/./static/js/runtime-main.08d3c691.js"
+  },
+  {
+    "revision": "02dccdc40a1610760d3767def7e46c7b",
+    "url": "/goit-react-hw-04-movies/./static/media/depotemplate-popcorn-soda.02dccdc4.jpg"
+  }
+]);
